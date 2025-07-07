23d4c68e884f0738e6dea3e999b9cf6dc0f56444
Felipe <guevara2000n@gmail.com>
2025-07-07 06:34:25 -0400

    Codificar tree.h y tree.cpp

--- Codigo-Fuente/src/tree.cpp
+++ Codigo-Fuente/src/tree.cpp
@@ -1,24 +1,6 @@
 #include "tree.h"
 using namespace std;
 
-/*
-Para indiceNodoHijo() se puede dar el parámtero indice_insertar si se quiere recibir
-el índice donde *estaría* para mantener el orden del vector hijos, 
-para poder usar dentro de insertarRuta().
-
-Tambien buscarNodoPadre() admite el parámetro indice_insertar ya que
-hará llamadas a indiceNodoHijo() para pasar de un nivel del árbol al siguiente
-
-El fundamento de estas decisiones es que tanto insertarRuta(), buscarNodoTipo() como eliminarRuta()
-se benefician de tener funciones para saber donde está un nodo hijo pero requieren 
-de información ligeramente distinta para ser aprovechadas
-
-insertarRuta() permite la insercion de rutas aun si no existen todos los directorios previos
-ej:
-ruta a insertar: x/y/z/1
-directorio encontrado: x
-crea el directorio y, luego el z e inserta el archivo 1
-*/
 
 NodoArbol* crearArbol() {
     NodoArbol* nodo = new NodoArbol;
@@ -30,7 +12,7 @@ NodoArbol* crearArbol() {
 NodoArbol* crearNodo(const string& nombre) {
     NodoArbol* nodo = new NodoArbol;
     nodo->nombre = nombre;
-    return;
+    return nodo;
 }
 
 
@@ -40,11 +22,10 @@ void destruirArbol(NodoArbol* nodo) {
     delete nodo;
 }
 
-
-// TODO: Como retorno la posición donde debería estar?
-int indiceNodoHijo(vector<NodoArbol*> v, const string& nombre, bool indice_insertar = false) {
+// FIXME: Qué ocurre si el vector está vacío?
+int indiceNodoHijo(vector<NodoArbol*> v, const string& nombre, bool indice_insertar) {
     int izq = 0;
-    int der = v.size() - 1;
+    int der = int(v.size()) - 1;
 
     while (izq <= der) {
         int med = izq + (der - izq) / 2;
@@ -52,12 +33,12 @@ int indiceNodoHijo(vector<NodoArbol*> v, const string& nombre, bool indice_inser
         string actual = v[med]->nombre;
         int i = 0;
         
-        while (i < min(actual.size(), nombre.size()) && actual[i] == nombre[i]) {
+        while (i < min(int(actual.size()), int(nombre.size())) && actual[i] == nombre[i]) {
             i++;
         }
 
         // si se recorrió una o dos de las palabras
-        if (i >= min(actual.size(), nombre.size())) {
+        if (i >= min(int(actual.size()), int(nombre.size()))) {
             if (actual.size() == nombre.size()) {
                 if (indice_insertar) {
                     return -1;
@@ -82,16 +63,14 @@ int indiceNodoHijo(vector<NodoArbol*> v, const string& nombre, bool indice_inser
     }
 
     if (indice_insertar) {
-        return ; // Qué cosa?
+        return izq;
     }
 
     return -1;
 }
 
-// TODO: probar con punteros normales
-// a lo mejor el puntero doble es solo con las clases
-// a lo mejor para eliminar si o si se necesita una referencia al puntero a la raiz
-NodoArbol** buscarNodoPadre(NodoArbol* nodo, const string& ruta, bool busca_insertar = false) {
+
+pair<NodoArbol**, const string&> buscarNodoPadre(NodoArbol* nodo, const string& ruta, bool busca_ancestro) {
     
     int idx_hijo;
     size_t primer_sep = ruta.find('/');
@@ -101,109 +80,68 @@ NodoArbol** buscarNodoPadre(NodoArbol* nodo, const string& ruta, bool busca_inse
 
     // si solo queda el hijo en la ruta
     if (primer_sep == string::npos) {
-        return &nodo;
+        return {&nodo, ruta};
     }
 
-    // está el siguiente nodo en el árbol?
     idx_hijo = indiceNodoHijo(nodo->hijos, nombre_base);
     
+    // está el siguiente nodo en el árbol?
     if (idx_hijo == -1) {
-        if (busca_insertar) {
+        if (busca_ancestro) {
             // retorna el ancestro
-            return &nodo;
+            return {&nodo, ruta};
         }
-        return nullptr;
+        return {nullptr, ruta}; // debería retornar un string vacío pero da error
     }
         
     string siguiente_ruta = ruta.substr(primer_sep+1);
-    return buscarNodoPadre(nodo->hijos[idx_hijo], siguiente_ruta);
+    return buscarNodoPadre(nodo->hijos[idx_hijo], siguiente_ruta, busca_ancestro);
 }
 
-// TODO
+
 bool insertarRuta(NodoArbol* nodo, const string& ruta) {
 
-    NodoArbol** ancestro = buscarNodoPadre(nodo, ruta, true);
+    auto [ancestro, ruta_restante] = buscarNodoPadre(nodo, ruta, true);
 
     if (*ancestro == nullptr) {
         return false;
     }
 
-    // ej:
-    // ruta: x/y/z
-    // ancestro: x
-    // ruta_restante -> y/z
-    // no sería necesario si pudieramos saber el nivel de profundidad al que llegó buscarNodoPadre
+    size_t primer_sep = ruta.find('/');
+    string nombre_base = ruta_restante.substr(0, primer_sep);
 
-    // si ancestro no es nulo, insertarmos en el vector de hijos la ruta correspondiente (cual?)
-    // si queda más ruta, creamos un nodo como nieto y lo agregamos al vector hijos del hijo
-    // repetir si queda más ruta
+    int idx_hijo = indiceNodoHijo((*ancestro)->hijos, nombre_base, true);
+    if (idx_hijo == -1) {
+        return false;   // ya está insertado
+    } 
 
+    NodoArbol* nodo_hijo = crearNodo(nombre_base);
 
+    (*ancestro)->hijos.insert((*ancestro)->hijos.begin() + idx_hijo, nodo_hijo);
 
-    // ya que existe insertarRuta, podría también existir bool insertarNodo() que asume estamos en el nodo padre
-    // probablemente innecesario ya que cumpliría la misma función que insertarRuta() llamado con otro argumento de nodo
-    // insertarNodo() sería iterativo y muy simple
+    string nombres_siguientes = ruta_restante.substr(primer_sep + 1);
+    while (!nombres_siguientes.empty()) {
+        NodoArbol** padre = &(nodo_hijo);
 
-    // encuentra el nodo padre
-    // caso 1: Solo se encuentra parcialmente la ruta
-        // crea un nodo y lo agrega al vector hijos del nodo padre
-        // como aun queda ruta repetimos sabiendo que el nodo padre ahora es 1
-        // retorna verdadero
-        // EJ:
-            // Queremos insertar x/1/a
-            // Existe x y no tiene un hijo 1
-            // ...
-    // caso 2: Se encuentra completamente la ruta
-        // retorna falso
-    
-        // NodoArbol* hijo = crearNodo(nombre_base);
-    //  ...inserta el hijo de manera que el vector hijos quede ordenado
-    // return true;
+        primer_sep = nombres_siguientes.find('/');
+        nombre_base = nombres_siguientes.substr(0, primer_sep);
+
+        nodo_hijo = crearNodo(nombre_base);
+        (*padre)->hijos.push_back(nodo_hijo);
+
+        nombres_siguientes = nombres_siguientes.substr(primer_sep + 1);
+    }  
+
+    return true;
 }    
 
 
-// TODO
 int buscarNodoTipo(NodoArbol* nodo, const string& ruta) {
-    NodoArbol** padre = buscarNodoPadre(nodo, ruta, false); 
+    
+    auto [padre, nombre_hijo] = buscarNodoPadre(nodo, ruta, false); 
 
     if (*padre == nullptr) {
         return 1;
-    }
-
-
-    // Encuentra el nodo padre
-    // Caso 1: Solo se encuentra parcialmente la ruta
-        // retorna 1 (Tipo vacio)
-        // EJ:
-        // Queremos buscar el nodo x/1/a
-            // xxiste x y no tiene un hijo 1 o existe x/1 y no tiene un hijo a
-            // retornamos x o x/1 respectivamente
-    // Caso 2: Se encuentra completamente la ruta
-        // caso 2.1: El nodo no tiene hijos
-            // retorna 0 (Tipo archivo)
-        // caso 2.2: El nodo tiene hijos
-            // retorna 2 (Tipo directorio)
-    
-}
-
-
-// TODO
-bool eliminarRuta(NodoArbol* nodo, const string& ruta) {
-
-    NodoArbol** padre = buscarNodoPadre(nodo, ruta, false);
-
-    if (*padre == nullptr) {
-        return false;
-    }
-
-    string nombre_hijo;
-    
-    size_t ultimo_sep = ruta.rfind('/');
-    if (ultimo_sep == string::npos) {
-        nombre_hijo = ruta;
-    }
-    else {
-        nombre_hijo = ruta.substr(ultimo_sep+1);
     }
 
     int idx_hijo = indiceNodoHijo((*padre)->hijos, nombre_hijo, false);
@@ -211,13 +149,29 @@ bool eliminarRuta(NodoArbol* nodo, const string& ruta) {
         return false;
     }
 
-    ((*padre)->hijos.erase((*padre)->hijos.begin() + idx_hijo));
+    if ((*padre)->hijos[idx_hijo]->hijos.size() == 0) {
+        return 0;
+    }
+
+    return 2;
+}
+
+
+bool eliminarRuta(NodoArbol* nodo, const string& ruta) {
+
+    auto [padre, nombre_hijo] = buscarNodoPadre(nodo, ruta, false);
+
+    if (*padre == nullptr) {
+        return false;
+    }
+
+    int idx_hijo = indiceNodoHijo((*padre)->hijos, nombre_hijo, false);
+    if (idx_hijo == -1) {
+        return false;
+    }
+
+    (*padre)->hijos.erase((*padre)->hijos.begin() + idx_hijo);
     destruirArbol((*padre)->hijos[idx_hijo]);
-
-
-    // encuentra el nodo padre
-    // caso 1: solo se encuentra parcialmente la ruta
-        // retorna falso
-    // caso 2: se encuentra completament la ruta
-        // llama a destruirArbol() y retorna verdadero
+    
+    return true;
 }
